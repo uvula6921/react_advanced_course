181bf74eb097b501dda8d3a695d97f0600a3026b
kyungjoon.park <pkj6921@naver.com>
2021-07-05 11:57:53 +0900

    오타 수정

--- image-community/src/pages/PostList.js
+++ image-community/src/pages/PostList.js
@@ -7,7 +7,7 @@ const PostList = (props) => {
   const dispatch = useDispatch();
   const post_list = useSelector((state) => state.post.list);
   const user_info = useSelector((state) => state.user.user);
-  let is_me = React.useEffect(() => {
+  React.useEffect(() => {
     if (post_list.length === 0) {
       dispatch(postActions.getPostFB());
     }
